a05c02faec1502138963a25e4e5f7dda5957f92d
pajedatribo1 <66290856+pajedatribo1@users.noreply.github.com>
2020-10-06 19:12:21 -0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,140 @@
+function up1() {
+    document.querySelector('#img1').style.transform = "scale(1.25)";
+
+}
+
+function down1() {
+    document.querySelector('#img1').style.transform = "scale(1.0)";
+
+}
+
+function up2() {
+    document.querySelector('#img2').style.transform = "scale(1.25)";
+
+}
+
+function down2() {
+    document.querySelector('#img2').style.transform = "scale(1.0)";
+
+}
+
+function up3() {
+    document.querySelector('#img3').style.transform = "scale(1.25)";
+
+}
+
+function down3() {
+    document.querySelector('#img3').style.transform = "scale(1.0)";
+
+}
+
+function up4() {
+    document.querySelector('#img4').style.transform = "scale(1.25)";
+
+}
+
+function down4() {
+    document.querySelector('#img4').style.transform = "scale(1.0)";
+
+}
+
+function up5() {
+    document.querySelector('#img5').style.transform = "scale(1.25)";
+
+}
+
+function down5() {
+    document.querySelector('#img5').style.transform = "scale(1.0)";
+
+}
+
+function up6() {
+    document.querySelector('#img6').style.transform = "scale(1.25)";
+
+}
+
+function down6() {
+    document.querySelector('#img6').style.transform = "scale(1.0)";
+
+}
+
+function up7() {
+    document.querySelector('#img7').style.transform = "scale(1.25)";
+
+}
+
+function down7() {
+    document.querySelector('#img7').style.transform = "scale(1.0)";
+
+}
+
+function up8() {
+    document.querySelector('#img8').style.transform = "scale(1.25)";
+
+}
+
+function down8() {
+    document.querySelector('#img8').style.transform = "scale(1.0)";
+
+}
+
+function up9() {
+    document.querySelector('#img9').style.transform = "scale(1.25)";
+
+}
+
+function down9() {
+    document.querySelector('#img9').style.transform = "scale(1.0)";
+
+}
+
+function up10() {
+    document.querySelector('#img10').style.transform = "scale(1.25)";
+
+}
+
+function down10() {
+    document.querySelector('#img10').style.transform = "scale(1.0)";
+
+}
+
+function up11() {
+    document.querySelector('#img11').style.transform = "scale(1.25)";
+
+}
+
+function down11() {
+    document.querySelector('#img11').style.transform = "scale(1.0)";
+
+}
+
+function up12() {
+    document.querySelector('#img12').style.transform = "scale(1.25)";
+
+}
+
+function down12() {
+    document.querySelector('#img12').style.transform = "scale(1.0)";
+
+}
+
+function up13() {
+    document.querySelector('#img13').style.transform = "scale(1.25)";
+
+}
+
+function down13() {
+    document.querySelector('#img13').style.transform = "scale(1.0)";
+
+}
+
+
+function up14() {
+    document.querySelector('#img14').style.transform = "scale(1.25)";
+
+}
+
+function down14() {
+    document.querySelector('#img14').style.transform = "scale(1.0)";
+
+}
